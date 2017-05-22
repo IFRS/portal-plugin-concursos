dac46a7625aa4303c3efabb0383453663f381914
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-05-22 16:56:15 -0300

    Renomeação do plugin para mais semântica.

--- ifrs-portal-concursos.php
+++ ifrs-portal-concursos.php
@@ -1,7 +1,7 @@
 <?php
 defined('ABSPATH') or die('No script kiddies please!');
 /*
-Plugin Name: IFRS Portal Plugin Concursos
+Plugin Name: IFRS Portal Concursos
 Plugin URI:  https://github.com/IFRS/portal-plugin-concursos
 Description: Plugin para gerenciar Concursos.
 Version:     1.0.0
@@ -16,3 +16,4 @@ Domain Path: /lang
 require_once('taxonomy-single-term/class.taxonomy-single-term.php');
 require_once('concurso-status.php');
 require_once('concurso.php');
+require_once('permissions.php');
