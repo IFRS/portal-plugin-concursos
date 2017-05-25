f263e4aedd28fe174384eda807b5f9bf0db2567b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-05-25 08:23:25 -0300

    Ajuste na chave de identificação da role.

--- permissions.php
+++ permissions.php
@@ -3,7 +3,7 @@ add_action('init', function() {
     if (get_role( 'cadastrador_concursos' )) {
         remove_role( 'cadastrador_concursos' );
     }
-    add_role('cadastrador_cursos', __('Cadastrador de Concursos'), array(
+    add_role('cadastrador_concursos', __('Cadastrador de Concursos'), array(
         'read'                   => true,
         'upload_files'           => true,
 
