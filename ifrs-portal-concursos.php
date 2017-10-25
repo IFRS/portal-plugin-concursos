606da3cf5abdc7e06c9e4145b0fe26a20fb4f36a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-10-25 14:36:43 -0200

    Incremento de versão.

--- ifrs-portal-concursos.php
+++ ifrs-portal-concursos.php
@@ -4,7 +4,7 @@ defined('ABSPATH') or die('No script kiddies please!');
 Plugin Name: IFRS Portal Concursos
 Plugin URI:  https://github.com/IFRS/portal-plugin-concursos
 Description: Plugin para gerenciar Concursos.
-Version:     1.1.0
+Version:     1.2.0
 Author:      Ricardo Moro
 Author URI:  https://github.com/ricardomoro
 License:     GPL3
