c3c276e17be7237dd2735ebcef5a509ff23348fb
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-06-22 14:14:33 -0300

    Ajuste nas roles para os administradores

--- roles.php
+++ roles.php
@@ -14,6 +14,9 @@ function ifrs_portal_concursos_addRoles() {
     $admin->add_cap('edit_concursos');
     $admin->add_cap('delete_concursos');
     $admin->add_cap('assign_concurso_status');
+    $admin->add_cap('manage_concurso_status');
+    $admin->add_cap('edit_concurso_status');
+    $admin->add_cap('delete_concurso_status');
 
     if (!get_role('cadastrador_concursos')) {
         add_role('cadastrador_concursos', __('Cadastrador de Concursos'), array(
@@ -55,6 +58,9 @@ function ifrs_portal_concursos_removeRoles() {
     $admin->remove_cap('edit_concursos');
     $admin->remove_cap('delete_concursos');
     $admin->remove_cap('assign_concurso_status');
+    $admin->remove_cap('manage_concurso_status');
+    $admin->remove_cap('edit_concurso_status');
+    $admin->remove_cap('delete_concurso_status');
 
     if (get_role('cadastrador_concursos')) {
         remove_role('cadastrador_concursos');
